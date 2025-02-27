cd7f5a49e9eab0a84fe98518475c517948cbc926
maxrumbo <maxrumbo06@gmail.com>
2025-02-27 13:29:06 +0700

    PAIR 24

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,30 @@
-// NIM - Name
-// NIM - Name
+// 12S24037 - Maxwell Avinda Rumahorbo
+// 12S24020 - Joice Anastasya Napitupulu
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+typedef struct {
+    char str[61];
+} Input;
 
-  return 0;
+int main() {
+    Input input;
+    int i, len;
+
+    fgets(input.str, sizeof(input.str), stdin);
+    input.str[strcspn(input.str, "\n")] = 0;
+
+    len = strlen(input.str);
+
+    if (len >= 3 && len <= 60) {
+        for (i = 0; i < len; i += 3) {
+            int ascii = (input.str[i] - '0') * 100 + (input.str[i + 1] - '0') * 10 + (input.str[i + 2] - '0');
+            printf("%c", ascii);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,28 @@
-// NIM - Name
-// NIM - Name
+// 12S24037 - Maxwell Avinda Rumahorbo
+// 12S24020 - Joice Anastasya Napitupulu
 
 #include <stdio.h>
+#include <string.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+typedef struct {
+    char str[21];
+} Input;
 
-  return 0;
+int main() {
+    Input input;
+    int i, len;
+
+    fgets(input.str, sizeof(input.str), stdin);
+    input.str[strcspn(input.str, "\n")] = 0;
+
+    len = strlen(input.str);
+
+    for (i = 0; i < len; i++) {
+        int ascii = (int)input.str[i];
+        printf("%03d", ascii);
+    }
+
+    printf("\n");
+
+    return 0;
 }
